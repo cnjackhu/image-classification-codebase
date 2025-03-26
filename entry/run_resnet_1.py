663460e4343d3486e267dc1151a688c80c94775f
Jiahao Hu <huj0b@cn605-16-r.ibex.kaust.edu.sa>
2025-03-26 14:35:43 +0300

    seperate the resnet to 2 part, each part 2 kinds resnet config

---
+++ entry/run_resnet_1.py
@@ -0,0 +1,26 @@
+from codebase.config import get_args
+from codebase.main import main
+import wandb
+
+if __name__ == "__main__":
+    #main(get_args())
+    # Define sweep config
+    sweep_configuration = {
+        "method": "grid",
+        "name": "sweep",
+        "parameters": {
+            "sweep_model": {"values": [
+        'cifar10_resnet20','cifar10_resnet32',
+        # 'cifar10_vgg11_bn','cifar10_vgg13_bn','cifar10_vgg16_bn','cifar10_vgg19_bn',
+        # 'cifar10_repvgg_a0','cifar10_repvgg_a1','cifar10_repvgg_a2', 
+        #'cifar10_mobilenetv2_x0_5','cifar10_mobilenetv2_x0_75','cifar10_mobilenetv2_x1_0','cifar10_mobilenetv2_x1_4',
+        # 'cifar10_shufflenetv2_x0_5', 'cifar10_shufflenetv2_x1_0','cifar10_shufflenetv2_x1_5','cifar10_shufflenetv2_x2_0',
+         
+        ]},
+        },
+    }
+
+    sweep_id = wandb.sweep(sweep=sweep_configuration, project="result_3.23") #my-sweep
+    wrapped_main = lambda: main(get_args())
+         # Start sweep job.
+    wandb.agent(sweep_id, function=wrapped_main, count=100)
