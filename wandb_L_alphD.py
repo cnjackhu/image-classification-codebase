24f2cb0eb00cc111335cc0e5841a0df9ffc9dfc9
Jiahao Hu <huj0b@cn605-27-r.ibex.kaust.edu.sa>
2025-05-14 12:30:22 +0300

    update code to get figure for baseline with lamb=1

--- wandb_L_alphD.py
+++ wandb_L_alphD.py
@@ -8,21 +8,20 @@ metrics=["eval/L","eval/alphaD"]
 target_names = ["resnet56-reg:5,lamb:1.0", "resnet56-baseline"]
 df_l = pd.DataFrame()
 df_ir=pd.DataFrame()
+
 for run in runs:
     name = run.name
-    if name in target_names:
+    if name in target_names and run.config['lamb']==1.0:
         new_name = "SGD"
-        if run.config['wd'] != 0:
+        if run.config['wd'] > 0:
             new_name += "-L2"
         if run.config['data_aug'] == True:
             new_name += "-DA"
-        if run.config['reg'] != 0:
-            new_name += "-IR"
-        
+        if run.config['reg'] == 5:
+            new_name += "-IR"      
         history = run.history(keys=metrics)
         df_l[new_name] = history["eval/L"].values
         df_ir[new_name] = history["eval/alphaD"].values
-breakpoint()
 df_l.to_csv("L.csv", index=False)
 df_ir.to_csv("alphaD.csv",index=False)
-
+    
