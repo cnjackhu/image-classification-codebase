a2193f002527c824725f2e5315cb2059d3faab5a
Jiahao Hu <huj0b@cn605-27-r.ibex.kaust.edu.sa>
2025-05-21 16:41:09 +0300

    update the previous results

--- test.sh
+++ test.sh
@@ -1,18 +1,20 @@
 #!/bin/bash --login
-#SBATCH --time=3:00:00
+#SBATCH --time=1:00:00
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=a100:1
 #SBATCH --cpus-per-gpu=6
 #SBATCH --mem=64G
 #SBATCH --partition=batch
 #SBATCH --job-name=Rate_opti
-#SBATCH --output=./bin/%A_%a.out
-#SBATCH --error=./bin/%A_%a.out
-
+#SBATCH --output=./bin/%j.out
+#SBATCH --error=./bin/%j.out
 
 module purge
 eval "$(conda shell.bash hook)"
 conda activate torch
-
+# echo "Running CIFAR-10"
 python val_test_metric.py cifar10
-python val_test_metric.py cifar100
+# echo "finished Running CIFAR-10"
+# echo "Running CIFAR-100"
+# python val_test_metric.py cifar100
+# echo "finished Running CIFAR-100"
