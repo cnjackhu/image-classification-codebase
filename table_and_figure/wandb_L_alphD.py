b615f966792ab1b58d36bba41e2489da641c50a4
Jiahao Hu <huj0b@login510-22.ibex.kaust.edu.sa>
2025-08-01 20:10:42 +0300

    mv all the figure and table related files to table_and_figure folder

--- table_and_figure/wandb_L_alphD.py
+++ table_and_figure/wandb_L_alphD.py
@@ -1,27 +1,28 @@
-import wandb
 import pandas as pd
-#jackhu0119/ablation2/y6v3mpxx
+import wandb
+
+# jackhu0119/ablation2/y6v3mpxx
+# generate the L.csv and alphaD.csv for plot figure in the paper
 api = wandb.Api()
-entity,project = "jackhu0119", "ablation2"
+entity, project = "jackhu0119", "ablation2"
 runs = api.runs(f"{entity}/{project}")
-metrics=["eval/L","eval/alphaD"]
+metrics = ["eval/L", "eval/alphaD"]
 target_names = ["resnet56-reg:5,lamb:1.0", "resnet56-baseline"]
 df_l = pd.DataFrame()
-df_ir=pd.DataFrame()
+df_ir = pd.DataFrame()
 
 for run in runs:
     name = run.name
-    if name in target_names and run.config['lamb']==1.0:
+    if name in target_names and run.config["lamb"] == 1.0:
         new_name = "SGD"
-        if run.config['wd'] > 0:
+        if run.config["wd"] > 0:
             new_name += "-L2"
-        if run.config['data_aug'] == True:
+        if run.config["data_aug"] == True:
             new_name += "-DA"
-        if run.config['reg'] == 5:
-            new_name += "-IR"      
+        if run.config["reg"] == 5:
+            new_name += "-IR"
         history = run.history(keys=metrics)
         df_l[new_name] = history["eval/L"].values
         df_ir[new_name] = history["eval/alphaD"].values
 df_l.to_csv("L.csv", index=False)
-df_ir.to_csv("alphaD.csv",index=False)
-    
+df_ir.to_csv("alphaD.csv", index=False)
