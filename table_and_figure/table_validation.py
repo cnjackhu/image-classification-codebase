8d3300343930507144d76961c586105cab63192d
Jiahao Hu <huj0b@gpu101-09-l.ibex.kaust.edu.sa>
2025-11-19 21:08:38 +0300

    clean the code related to tex table generation

--- table_and_figure/table_validation.py
+++ table_and_figure/table_validation.py
@@ -271,11 +271,4 @@ def process_and_compare_summary(file_path: str, name: str,celoss:int):
     print("\nSummary table content:")
     print(summary_table)
 
-if __name__ == "__main__":
-# for cifar10 data
-    print("below is the result for cifar10:")
-    process_and_compare_summary("results_cifar10.csv", "cifar10_validation")
 
-# for cifar100 data
-    print("below is the result for cifar100:")
-    process_and_compare_summary("results_cifar100.csv", "cifar100_validation")
