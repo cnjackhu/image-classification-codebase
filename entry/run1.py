d7edaa38f527cdc91b3a9c9fb44cbea82fb72299
hjh <hujiahao0119@gmail.com>
2026-01-28 10:08:14 +0300

    adapt to reg

--- entry/run1.py
+++ entry/run1.py
@@ -45,7 +45,8 @@ if __name__ == "__main__":
     elif num_classes == 10:
         prefix = "cifar10_"
     else:
-        raise ValueError("Unsupported number of classes")
+        prefix = "reg_"+ args.conf.data.type_
+        print(prefix)
     # Selectively enable models (you can modify this list if needed)
     selected_models = base_models
     sweep_configuration["parameters"]["sweep_model"]["values"] = [
